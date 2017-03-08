2a11825860c9a0fd639a9322cbe0f41fb60b4d96
Masneri Stefano <stocastico@gmail.com>
2017-03-08 15:26:30 +0100

    Increase number of jobs in polynomial svm

--- Exercises/Exercise_3/part_3/problem3_3.py
+++ Exercises/Exercise_3/part_3/problem3_3.py
@@ -16,7 +16,7 @@ def doSVM(X_train, X_test, y_train, y_test, outputFile):
     print('------ Linear SVM -------')
     tuned_parameters_linear = {'kernel': ['linear'],
                                'C': [0.1, 0.5, 1, 5, 10, 50, 100]}
-    clf1 = GridSearchCV(SVC(), tuned_parameters_linear)   
+    clf1 = GridSearchCV(SVC(), tuned_parameters_linear, cv = 5)   
     clf1.fit(X_train, y_train)
     print(clf1.best_params_)
     bestScore = clf1.best_score_
@@ -28,7 +28,7 @@ def doSVM(X_train, X_test, y_train, y_test, outputFile):
                              'C': [0.1, 1, 3], 
                              'degree': [4, 5, 6],
                              'gamma': [0.1, 1]}
-    clf2 = GridSearchCV(SVC(), tuned_parameters_poly)   
+    clf2 = GridSearchCV(SVC(), tuned_parameters_poly, cv = 5, n_jobs = 8)   
     clf2.fit(X_train, y_train)
     print(clf2.best_params_)
     bestScore = clf2.best_score_
@@ -39,7 +39,7 @@ def doSVM(X_train, X_test, y_train, y_test, outputFile):
     tuned_parameters_rbf = {'kernel': ['rbf'],
                             'C': [0.1, 0.5, 1, 5, 10, 50, 100], 
                             'gamma': [0.1, 0.5, 1, 3, 6, 10]}
-    clf3 = GridSearchCV(SVC(), tuned_parameters_rbf)   
+    clf3 = GridSearchCV(SVC(), tuned_parameters_rbf, cv = 5)   
     clf3.fit(X_train, y_train)
     print(clf3.best_params_)
     bestScore = clf3.best_score_
@@ -49,7 +49,7 @@ def doSVM(X_train, X_test, y_train, y_test, outputFile):
 def doLogisticRegression(X_train, X_test, y_train, y_test, outputFile):
     print('------ Logistic Regression -------')
     tuned_parameters = {'C': [0.1, 0.5, 1, 5, 10, 50, 100]}
-    clf = GridSearchCV(LogisticRegression(), tuned_parameters)   
+    clf = GridSearchCV(LogisticRegression(), tuned_parameters, cv = 5)   
     clf.fit(X_train, y_train)
     print(clf.best_params_)
     bestScore = clf.best_score_
@@ -60,7 +60,7 @@ def doKNN(X_train, X_test, y_train, y_test, outputFile):
     print('------ KNN ------')
     tuned_parameters = {'n_neighbors': range(1, 51), 
                         'leaf_size': range(5, 61, 5)}
-    clf = GridSearchCV(KNeighborsClassifier(), tuned_parameters)   
+    clf = GridSearchCV(KNeighborsClassifier(), tuned_parameters, cv = 5)   
     clf.fit(X_train, y_train)
     print(clf.best_params_)
     bestScore = clf.best_score_
@@ -71,7 +71,7 @@ def doDecisionTree(X_train, X_test, y_train, y_test, outputFile):
     print('------ Decision tree ------')
     tuned_parameters = {'max_depth': range(1, 51), 
                         'min_samples_split ': range(1, 11)}
-    clf = GridSearchCV(DecisionTreeClassifier(), tuned_parameters)   
+    clf = GridSearchCV(DecisionTreeClassifier(), tuned_parameters, cv = 5)   
     clf.fit(X_train, y_train)
     print(clf.best_params_)
     bestScore = clf.best_score_
@@ -82,7 +82,7 @@ def doRandomForest(X_train, X_test, y_train, y_test, outputFile):
     print('------ Random forest ------')
     tuned_parameters = {'max_depth': range(1, 51), 
                         'min_samples_split ': range(1, 11)}
-    clf = GridSearchCV(RandomForestClassifier(), tuned_parameters)   
+    clf = GridSearchCV(RandomForestClassifier(), tuned_parameters, cv = 5)   
     clf.fit(X_train, y_train)
     print(clf.best_params_)
     bestScore = clf.best_score_
