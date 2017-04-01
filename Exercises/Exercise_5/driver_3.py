e68f400903b9c496b388333ccf9d4c2ff4fc60ab
Stocastico <stocastico@gmail.com>
2017-04-01 18:46:33 +0200

    Work on project 5

--- Exercises/Exercise_5/driver_3.py
+++ Exercises/Exercise_5/driver_3.py
@@ -1,5 +1,9 @@
+import os
+from random import shuffle
+
 train_path = "../resource/asnlib/public/aclImdb/train/" # use terminal to ls files under this directory
 test_path = "../resource/asnlib/public/imdb_te.csv" # test data for grade evaluation
+stopwords_path = "../resources/asnlib/public/aclImdb/stopwords.en.txt" # file containing common stopwords
 
 
 def imdb_data_preprocess(inpath, outpath="./", name="imdb_tr.csv", mix=False):
@@ -8,22 +12,63 @@ def imdb_data_preprocess(inpath, outpath="./", name="imdb_tr.csv", mix=False):
     imdb_tr.csv. Each text file in train_path should be stored 
     as a row in imdb_tr.csv. And imdb_tr.csv should have two 
     columns, "text" and label'''
-  pass
+
+  inpath = os.path.abspath(inpath) + '/'
+  outpath = os.path.abspath(outpath) + '/'
+
+  # create out file
+  with open(outpath + name, 'w') as f:
+    # write header
+    f.write("row_number, text, polarity")
+    # initialize output string
+    out = []
+    # now read all files. First positive...
+    for filename in os.listdir(inpath + "pos"):
+      with open(inpath + "pos/" + filename, 'r') as tmp:
+        out.append(tmp.read() + ", 1\n")
+    # ...then negative
+    for filename in os.listdir(inpath + "neg"):
+      with open(inpath + "neg/" + filename, 'r') as tmp:
+        out.append(tmp.read() + ", 0\n")
+    if mix: # shuffle
+      shuffle(out)
+    # and finally write to file, adding row numbers
+    row = 0
+    for line in out:
+      f.write(row, line)
+      row += 1
+  
+
+def imdb_remove_stopwords(inFile, outFile, stopFile):
+  ''' Remove all stopwords from file inFile and write the filtered file to
+  outFile'''
+  # Read all stopwords
+  with open(stopFile, 'r'):
+    stopwords = set(f.readLines())
+  # Open files 
+  with open(inFile, 'r') as inF, open(outFile, 'w') as outF:
+    text = inF.readLines()
+    text = ' '.join([word for word in text.split() if word not in stopWords])
+    outF.write(text)
   
 if __name__ == "__main__":
+  ''' prepare data '''
+  imdb_data_preprocess(train_path, './', 'imdb_tr0.csv', True)
+  imdb_remove_stopwords('./imdb_tr0.csv', './imdb_tr.csv', stopwords_path)
+
   '''train a SGD classifier using unigram representation,
     predict sentiments on imdb_te.csv, and write output to
     unigram.output.txt'''
     
-    '''train a SGD classifier using bigram representation,
+  '''train a SGD classifier using bigram representation,
     predict sentiments on imdb_te.csv, and write output to
     unigram.output.txt'''
      
-     '''train a SGD classifier using unigram representation
+  '''train a SGD classifier using unigram representation
      with tf-idf, predict sentiments on imdb_te.csv, and write 
      output to unigram.output.txt'''
     
-     '''train a SGD classifier using bigram representation
+  '''train a SGD classifier using bigram representation
      with tf-idf, predict sentiments on imdb_te.csv, and write 
      output to unigram.output.txt'''
-     pass
+  pass
